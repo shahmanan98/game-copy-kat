d75fefda5eb552b38d78e2b113bcd657d500a0d7
AnMan9801 <AnMan9801@users.noreply.github.com>
2020-11-22 14:15:57 +0530

    refactor : change sund js

--- public/js/sound/sound.js
+++ public/js/sound/sound.js
@@ -1,14 +1,20 @@
 let mute = false;
+let boxes = [];
+boxes[0] = PIXI.sound.add('music', '/audio/Pink.mp3');
+boxes[1] = PIXI.sound.add('music', '/audio/Blue.mp3');
+boxes[2] = PIXI.sound.add('music', '/audio/Green.mp3');
+boxes[3] = PIXI.sound.add('music', '/audio/Yellow.mp3');
+let click = PIXI.sound.add('music', '/audio/buttonClick.mp3');
+let muteSound = PIXI.sound.add('music', '/audio/muteButton.mp3');
+let incorrect = PIXI.sound.add('music', '/audio/Wrong.mp3');
 
 export function playIncorrect() {
-    let incorrect = PIXI.sound.add('music', '/audio/Wrong.mp3');
     if (!mute) {
         incorrect.play();
     }
 }
 
 export function playClick() {
-    let click = PIXI.sound.add('music', '/audio/buttonClick.mp3');
     if (!mute) {
         click.play();
     }
@@ -16,17 +22,11 @@ export function playClick() {
 
 export function playMute() {
     mute = !mute;
-    let click = PIXI.sound.add('music', '/audio/muteButton.mp3');
     if (!mute) {
-        click.play();
+        muteSound.play();
     }
 }
 
-let boxes = [];
-boxes[0] = PIXI.sound.add('music', '/audio/Pink.mp3');
-boxes[1] = PIXI.sound.add('music', '/audio/Blue.mp3');
-boxes[2] = PIXI.sound.add('music', '/audio/Green.mp3');
-boxes[3] = PIXI.sound.add('music', '/audio/Yellow.mp3');
 export function playBoxes(index) {
     if (!mute) {
         boxes[index].play();
